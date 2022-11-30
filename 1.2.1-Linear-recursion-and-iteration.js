f1a7b0551cefaed4db8fbb8979ebca2150cb6553
Source Academy User <No public email provided>
2022-11-30 20:49:45 -0300

    2

--- 1.2.1-Linear-recursion-and-iteration.js
+++ 1.2.1-Linear-recursion-and-iteration.js
@@ -101,3 +101,40 @@ las variables de estado, que indica "donde está el proceso" en la negociación
 de la cadena de operaciones diferidas. Cuanto más larga sea la cadena, más
 información debe ser mantenida.
 */
+
+/*
+Al contrastar iteración y recursión debe tenerse la precacución de no confundir
+la noción de un PROCESO RECURSIVO con la noción de una FUNCIÓN RECURSIVA.
+
+Cuando describimos una función como recursiva, nos referimos al hecho sintáctico
+de que la función hace referencia (directa o indirectamente) a sí misma como
+función.
+
+Cuando describimos un proceso como siguiendo un patrón, por ejemplo, recursivo
+lineal, estamos hablando de cómo el proceso evoluciona, no de la sintaxis de una
+función.
+
+Puede resultar perturbador referirse a una función recursiva como fact_iter
+generadora de un proceso iterativo. Sin embargo, el proceso es realmente
+iterativo: su estado está capturado completamente por sus tres variables de
+estado, y un intérprete solo necesita llevar cueta de tres nombres para ejecutar
+el proceso.
+*/
+
+/*
+Una implementación que ejecute un proceso iterativo en espacio constante,
+inclusive si el proceso iterativo es descrito mediante una función recursiva
+se denomina tail-recursive.
+
+La mayoría de la implementaciones de lenguajes comunes como C, Java y Python
+están diseñadas de modo que la interpretación de cualquier función recursiva
+consume una cantidad de memoria que crece con el número de llamadas, aún si el
+proceso descrito es, en principio, iterativo. En consecuencia, estos lenguajes
+pueden describir procesos iterativos solo mediante "looping constructs"
+especiales, tales como do, repeat, until, for, y while.
+
+Con una omplementación tail-recursive, las iteraciones pueden expresarse
+mediante los mecanismos ordinarios de llamadas de funciones, de modo que los 
+constructos especiales de iteración solo sirven de syntactic sugar.
+*/
+
