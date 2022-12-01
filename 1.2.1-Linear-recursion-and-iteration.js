b670bebb8b4743c6d7b764e4af66c2b32c09e7e6
Source Academy User <No public email provided>
2022-12-01 01:48:03 -0300

    3

--- 1.2.1-Linear-recursion-and-iteration.js
+++ 1.2.1-Linear-recursion-and-iteration.js
@@ -79,7 +79,7 @@ fact_iter(720, 7, 6);
 Este proceso no crece ni decrece.
 Se trata de un Proceso Iterativo: su estado
 puede ser resumido por un número fijo de variables de estado, junto a una
-regla fija que describe cómo las variables dde estado deben actualizarse
+regla fija que describe cómo las variables de estado deben actualizarse
 mientras el proceso se mueve de estado a estado, y (opcionalmente) una prueba
 final que especifica condiciones bajo las cuales el proceso debe terminar.
 Calculando n!, el número de pasos requeridos crece linealmente con n. Este
@@ -138,3 +138,178 @@ mediante los mecanismos ordinarios de llamadas de funciones, de modo que los
 constructos especiales de iteración solo sirven de syntactic sugar.
 */
 
+
+// 1.9
+
+function inc(x) {
+    return x+1;
+}
+function dec(x) {
+    return x-1;
+}
+
+
+/*
+
+function plus(a, b) {
+    return a === 0 ? b : inc(plus(dec(a), b));
+}
+
+plus(4, 5);
+inc(plus(3, 5));
+inc(inc(plus(2, 5)));
+inc(inc(inc(plus(1, 5))));
+inc(inc(inc(inc(plus(0, 5)))));
+inc(inc(inc(inc(5))));
+inc(inc(inc(6)));
+inc(inc(7));
+inc(8);
+9
+
+Proceso recursivo, se acumulan operaciones inc pospuestas.
+El espacio es proporcional al parámetro a.
+
+*/
+
+
+/*
+
+function plus(a, b) {
+    return a === 0 ? b : plus(dec(a), inc(b));
+}
+
+plus(4, 5);
+plus(3, 6);
+plus(2, 7);
+plus(1, 8);
+plus(0, 9);
+9
+
+A pesar de ser decrito por una función recursiva, es un proceso iterativo,
+el espacio es constante.
+
+*/
+
+
+// 1.10
+function A(x, y) {
+    return y === 0
+           ? 0
+           : x === 0
+           ? 2 * y
+           : y === 1
+           ? 2
+           : A(x - 1, A(x, y - 1));
+}
+
+// function f(n) {
+//     return A(0, n);
+// }
+// A(0, n)
+// ->
+// 2 * n
+
+function h(n) {
+    return A(2, n);
+}
+A(2, n)
+A(1, A(2, n-1))
+A(1, A(1, A(2, n-2)))
+A(1, A(1, A(1, A(2, n-3))))
+...
+A(1, A(1, A(1, ... *(n-1) ... A(2, n-(n-1)))))
+A(1, A(1, A(1, ... *(n-1) ... A(2, 1))))
+A(1, A(1, A(1, ... *(n-1) ... 2)))
+
+A(1, 2)
+A(0, A(1, 1))
+A(0, 2)
+2
+
+A(1, A(1, 2))
+A(1, A(0, A(1, 1)))
+A(1, 4)
+A(1, A(0, 3))
+A(1, 6)
+A(1, A(0, A(1, 5)))
+
+
+
+h(4);
+
+function g(n) {
+    return A(1, n);
+}
+// A(1, n)
+// A(0, A(1, n-1))
+// A(0, A(0, A(1, n-2)))
+// A(0, A(0, A(0, A(1, n-3))))
+// ...
+// A(0, A(0, A(0, ... *(n-1) ...  A(1, n-(n-1)))))
+// A(0, A(0, A(0, ... *(n-1) ... A(1, 1))))
+// A(0, A(0, A(0, ... *(n-1) ... 2)))
+// ->
+// 2 ^(n-1) * 2 ->
+// 2^n
+
+
+
+
+
+
+// A(1, 10)
+// A(0, A(1, 9))
+// A(0, A(0, A(1, 8)))
+// A(0, A(0, A(0, A(1, 7))))
+// A(0, A(0, A(0, A(0, A(1, 6)))))
+// A(0, A(0, A(0, A(0, A(0, A(1, 5))))))
+// A(0, A(0, A(0, A(0, A(0, A(0, A(1, 4)))))))
+// A(0, A(0, A(0, A(0, A(0, A(0, A(0, A(1, 3))))))))
+// A(0, A(0, A(0, A(0, A(0, A(0, A(0, A(0, A(1, 2)))))))))
+// A(0, A(0, A(0, A(0, A(0, A(0, A(0, A(0, A(0, A(1, 1))))))))))
+// A(0, A(0, A(0, A(0, A(0, A(0, A(0, A(0, A(0, 2)))))))))
+// A(0, A(0, A(0, A(0, A(0, A(0, A(0, A(0, 4))))))))
+// A(0, A(0, A(0, A(0, A(0, A(0, A(0, 8)))))))
+// A(0, A(0, A(0, A(0, A(0, A(0, 16))))))
+// A(0, A(0, A(0, A(0, A(0, 32)))))
+// A(0, A(0, A(0, A(0, 64))))
+// A(0, A(0, A(0, 128)))
+// A(0, A(0, 256))
+// A(0, 512)
+// 1024
+
+
+// A(2, 4)
+// A(1, A(2, 3))
+// A(1, A(1, A(2, 2)))
+// A(1, A(1, A(1, A(2, 1))))
+// A(1, A(1, A(1, 2)))
+// A(1, A(1, A(0, A(1, 1))))
+// A(1, A(1, A(0, 2)))
+// A(1, A(1, 4))
+// A(1, A(0, A(1, 3)))
+// A(1, A(0, A(0, A(1, 2))))
+// A(1, A(0, A(0, A(0, A(1, 1)))))
+// A(1, A(0, A(0, A(0, 2))))
+// A(1, A(0, A(0, 4)))
+// A(1, A(0, 8))
+// A(1, 16)
+// A(0, A(1, 15))
+// A(0, A(0, A(1, 14)))
+// A(0, A(0, A(0, A(1, 13))))
+// A(0, A(0, A(0, A(0, A(1, 12)))))
+// ...
+// 65536
+
+
+// A(3, 3)
+// A(2, A(3, 2))
+// A(2, A(2, A(3, 1)))
+// A(2, A(2, 2))
+// A(2, A(1, A(2, 1)))
+// A(2, A(1, 2))
+// A(2, A(0, A(1, 1)))
+// A(2, A(0, 2))
+// A(2, 4)
+// ...
+// 65536
